a9a78efb595ef2a64916f146cd3f99813481bfe7
Ytaro Furukawa <furukawa_yutaro@liginc.co.jp>
2016-10-12 20:19:12 +0900

    raindrops

---
+++ ligblog-raindrops/hoge.js
@@ -0,0 +1,11 @@
+$(function(){
+	let $hoge = $('.hoge');
+	$hoge.raindrops({
+		color:'#fff',
+		waveHeight: 200,
+		canvasHeight:300,
+		rippleSpeed: 0.01,
+		rippleSpeed: 0.05, 
+		density: 0.04
+	});
+});
